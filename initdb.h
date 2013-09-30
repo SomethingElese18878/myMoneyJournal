1645783585f6ad60c4084d2c09b69fe3a6606566
norman-walker <norman.walker@avira.com>
2013-09-30 13:00:12 +0200

    insert of data put from ELSE into IF-section, when creating a new database.

--- initdb.h
+++ initdb.h
@@ -43,27 +43,26 @@ QSqlError initDb()
         if (tables.contains("booking", Qt::CaseInsensitive) && tables.contains("accounts", Qt::CaseInsensitive))
             std::cout << "TABLES CONTAINS!!!!" << std::endl;
             return QSqlError();
+    } else{
+        //Create tables ACCOUNTS & BOOKING
+        QSqlQuery q;
+        if (!q.exec(QLatin1String("create table accounts(id integer primary key, name varchar)"))) //total for lbl?
+            return q.lastError();
+        if (!q.exec(QLatin1String("create table booking(id integer primary key, description varchar, price integer)")))
+            return q.lastError();
+
+        //    Insert datas into table ACCOUNTS
+        //    if (!q.prepare(QLatin1String("insert into accounts(name) values(?)")))
+        //        return q.lastError();
+        //    addAccount(q, QLatin1String("norman"));
+        //    addAccount(q, QLatin1String("test"));
+
+            //Insert datas into table BOOKING
+        //    if (!q.prepare(QLatin1String("insert into booking(description, price) values(?, ?)")))
+        //        return q.lastError();
+        //    addBooking(q, QLatin1String("Gehalt"), 750);
+        //    addBooking(q, QLatin1String("Food"), -10);
     }
-
-
-    //Create tables ACCOUNTS & BOOKING
-    QSqlQuery q;
-    if (!q.exec(QLatin1String("create table accounts(id integer primary key, name varchar)"))) //total for lbl?
-        return q.lastError();
-    if (!q.exec(QLatin1String("create table booking(id integer primary key, description varchar, price integer)")))
-        return q.lastError();
-
-//    Insert datas into table ACCOUNTS
-//    if (!q.prepare(QLatin1String("insert into accounts(name) values(?)")))
-//        return q.lastError();
-//    addAccount(q, QLatin1String("norman"));
-//    addAccount(q, QLatin1String("test"));
-
-    //Insert datas into table BOOKING
-//    if (!q.prepare(QLatin1String("insert into booking(description, price) values(?, ?)")))
-//        return q.lastError();
-//    addBooking(q, QLatin1String("Gehalt"), 750);
-//    addBooking(q, QLatin1String("Food"), -10);
 
     return QSqlError();
 }
